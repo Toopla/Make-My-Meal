ef30bcf52c4458a2937812f445c89b73b1b07777
GuiGui552 <lisseguillaume@gmail.com>
2023-04-03 15:06:47 +0200

    page d'accueil (non-connecté) #5

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -6,11 +6,30 @@ const Home = (props) => {
         name = props.cookies.BearerToken.name;
     }
 
-    return (
-        <div style={{marginLeft: 50, marginTop: 20}}>
-            <div style={{fontSize: 30, fontWeight: 'bold'}}>{name === undefined ? 'Bienvenue sur le projet Javascript' : 'Bienvenue ' + name}</div>
-        </div>
-    )
+    if (name === undefined){
+        return (
+            <div>
+                <div className={"logo-content"}>
+                    <img className={"logo"} src="images/Make_My_Meal.png" alt="logo"/>
+                </div>
+
+                <div className={"description-content"}>
+                    <p className={"description"}>
+                        Vous pouvez réserver un chef cuisinier pour qu’il vienne cuisiner à domicile en fonction de l’heure du repas (petit déjeuner, déjeuner, goûter, repas)
+                        et choisir le chef qu’il souhaite réserver parmi une liste de chefs.
+                        Quant à lui, le chef pourra accepter ou refuser votre réservation.
+                    </p>
+                </div>
+            </div>
+        )
+    }else {
+        return (
+            <div style={{marginLeft: 50, marginTop: 20}}>
+                <div style={{fontSize: 30, fontWeight: 'bold'}}>Bienvenue {name}</div>
+            </div>
+        )
+    }
+
 }
 
 export default Home;
