e03c4b69b6fd3cc91ee7b6a00a4f30d4d0a10764
Biboi1 <95025072+Biboi1@users.noreply.github.com>
2023-04-04 10:21:29 +0200

    barre de recherche #6

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,32 +1,34 @@
-import React, {useEffect, useState} from "react";
-import {get_users_chefs} from "../services/api";
+import React, { useEffect, useState } from "react";
+import { get_users_chefs } from "../services/api";
 
 const Home = (props) => {
-    const [dataUsersChefs,setDataUsersChefs] = useState([]);
+    const [dataUsersChefs, setDataUsersChefs] = useState([]);
+    const [searchCategory, setSearchCategory] = useState("");
+    const [searchName, setSearchName] = useState("");
 
     const usersChefs = () => {
-        if (props.cookies.BearerToken){
+        if (props.cookies.BearerToken) {
             get_users_chefs(props.cookies.BearerToken.token).then((values) => {
                 setDataUsersChefs(values);
                 console.log(values);
-            })
+            });
         }
-    }
+    };
 
     useEffect(() => {
         usersChefs();
     }, []);
 
     let name;
-    if(props.cookies.BearerToken) {
+    if (props.cookies.BearerToken) {
         name = props.cookies.BearerToken.name;
     }
 
-    if (name === undefined){
+    if (name === undefined) {
         return (
             <div>
                 <div className={"logo-content"}>
-                    <img className={"logo"} src="images/Make_My_Meal.png" alt="logo"/>
+                    <img className={"logo"} src="images/Make_My_Meal.png" alt="logo" />
                 </div>
 
                 <div className={"description-content"}>
@@ -37,21 +39,50 @@ const Home = (props) => {
                     </p>
                 </div>
             </div>
-        )
-    }else {
+        );
+    } else {
+        const filteredChefs = dataUsersChefs.filter(
+            (chef) =>
+                chef.spec.toLowerCase().includes(searchCategory.toLowerCase()) &&
+                `${chef.firstname} ${chef.lastname}`
+                    .toLowerCase()
+                    .includes(searchName.toLowerCase())
+        );
+
         return (
-            <div style={{marginLeft: 50, marginTop: 20}}>
-                <div style={{fontSize: 30}}>Liste des chefs</div>
+            <div style={{ marginLeft: 50, marginTop: 20 }}>
+                <div style={{ fontSize: 30 }}>Liste des chefs</div>
+                <div style={{ marginBottom: 10 }}>
+                    <label htmlFor="category">Catégorie :</label>
+                    <input
+                        type="text"
+                        id="category"
+                        value={searchCategory}
+                        onChange={(e) => setSearchCategory(e.target.value)}
+                    />
+                </div>
+                <div style={{ marginBottom: 10 }}>
+                    <label htmlFor="name">Nom :</label>
+                    <input
+                        type="text"
+                        id="name"
+                        value={searchName}
+                        onChange={(e) => setSearchName(e.target.value)}
+                    />
+                </div>
                 <ul>
-                    {dataUsersChefs.map((prop, key) =>{
-                        return(
-                            <li key={key}>{prop.firstname} {prop.lastname} {prop.adresse} {prop.mail} {prop.role} {prop.spec}</li>
-                        )
+                    {filteredChefs.map((prop, key) => {
+                        return (
+                            <li key={key}>
+                                {prop.firstname} {prop.lastname} {prop.adresse} {prop.mail}{" "}
+                                {prop.role} {prop.spec}
+                            </li>
+                        );
                     })}
                 </ul>
             </div>
-        )
+        );
     }
-}
+};
 
 export default Home;
