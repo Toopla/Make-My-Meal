cb8d1b8d5cd515877c961ac9aea83771179d25f2
Enzo Blois <enzoblois4@gmail.com>
2023-04-05 13:46:01 +0200

    :construction: ajout explication test front #9

--- front/src/components/__tests__/Navbar.test.js
+++ front/src/components/__tests__/Navbar.test.js
@@ -52,3 +52,13 @@ describe('NavBar', () => {
         expect(props.removeCookie).toHaveBeenCalledWith('BearerToken', '/');
     });
 });
+
+/*
+Dans ce test, nous créons des objets props pour les tests et un objet navigate qui est une fonction fictive pour simuler la fonction useNavigate du composant. Nous utilisons beforeEach pour réinitialiser les objets avant chaque test et afterEach pour réinitialiser les fonctions fictives après chaque test.
+
+    Ensuite, nous testons si le logo est bien affiché sur la page, si la navigation se fait correctement lorsqu'on clique sur les boutons "Accueil" et "Items", et si la navigation se fait correctement lorsqu'on clique sur le bouton "Connexion / Créer compte" ou sur le bouton "Déconnexion" selon si l'utilisateur est connecté ou non.
+
+Nous testons également si la fonction removeCookie est bien appelée avec les bons paramètres lorsqu'on clique sur le bouton "Déconnexion" lorsqu'un utilisateur est connecté.
+
+    Notez que nous utilisons la méthode jest.mock pour remplacer la fonction useNavigate par une fonction fictive pour simuler le comportement de la fonction useNavigate.
+*/
