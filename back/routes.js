03091093da84b2b7a39397573bc42bf4fed40efa
Toopla <alexis.grislin@outlook.fr>
2023-04-06 12:40:33 +0200

    Reservation Client #37

--- back/routes.js
+++ back/routes.js
@@ -29,8 +29,50 @@ routes.get('/chefs/liste', auth.authenticate(), (req, res) => {
     })
 })
 
+routes.get('/reservation/:id', auth.authenticate(), (req, res) => {
+    db.all('SELECT reserv_id AS id, reserv_id_client AS id_client, reserv_id_chef AS id_chef, reserv_name_chef AS name_chef, reserv_name_client AS name_client, reserv_adresse AS adresse, reserv_jour AS jour, reserv_creneau AS creneau, reserv_photo AS photo FROM reservation WHERE reserv_id_client=$id_client OR reserv_id_chef=$id_chef ORDER BY jour ASC', {$id_client: req.params.id, $id_chef: req.params.id}, (err, rows) => {
+        if(err) {
+            return res.json(err).status(500);
+        } else {
+            return res.json(rows).status(200);
+        }
+    })
+})
+
+routes.post('/reservation', auth.authenticate(), (req, res) => {
+    db.run('INSERT INTO reservation(reserv_id_client, reserv_id_chef, reserv_name_chef, reserv_name_client, reserv_adresse, reserv_jour, reserv_creneau, reserv_photo) VALUES ($id_client, $id_chef, $name_chef, $name_client, $adresse, datetime($jour, "localtime"), $creneau, $photo)', {
+        $id_client: req.body.id_client,
+        $id_chef: req.body.id_chef,
+        $name_chef: req.body.name_chef,
+        $name_client: req.body.name_client,
+        $adresse: req.body.adresse,
+        $jour: req.body.jour,
+        $creneau: req.body.creneau,
+        $photo: req.body.photo
+    }, (err) => {
+        if(err) {
+            return res.json(err).status(500);
+        } else {
+            return res.status(200);
+        }
+    })
+})
+
+routes.delete('/reservation/:id_client/:id', auth.authenticate(), (req, res) => {
+    db.run('DELETE FROM reservation WHERE reserv_id_client=$id_client AND reserv_id=$id', {
+        $id_client: req.params.id_client,
+        $id: req.params.id
+    }, (err) => {
+        if(err) {
+            return res.json(err).status(500);
+        } else {
+            return res.status(200);
+        }
+    })
+})
+
 routes.get('/planning/:id', auth.authenticate(), (req, res) => {
-    db.all('SELECT plan_id AS id, plan_id_chef AS id_chef, plan_jour AS jour, plan_petit_dejeuner AS petit_dejeuner, plan_dejeuner AS dejeuner, plan_gouter AS gouter, plan_repas AS repas FROM planning WHERE plan_id_chef=$id_chef', {$id_chef: req.params.id}, (err, rows) => {
+    db.all('SELECT plan_id AS id, plan_id_chef AS id_chef, plan_jour AS jour, plan_petit_dejeuner AS petit_dejeuner, plan_dejeuner AS dejeuner, plan_gouter AS gouter, plan_repas AS repas FROM planning WHERE plan_id_chef=$id_chef ORDER BY plan_jour ASC', {$id_chef: req.params.id}, (err, rows) => {
         if(err) {
             return res.json(err).status(500);
         } else {
@@ -40,35 +82,48 @@ routes.get('/planning/:id', auth.authenticate(), (req, res) => {
 })
 
 routes.post('/planning', auth.authenticate(), (req, res) => {
-    db.get('INSERT INTO planning(plan_id_chef, plan_jour, plan_petit_dejeuner, plan_dejeuner, plan_gouter, plan_repas) VALUES ($id_chef, datetime($jour, "localtime"), $petit_dejeuner, $dejeuner, $gouter, $repas) returning plan_id', {
+    db.run('INSERT INTO planning(plan_id_chef, plan_jour, plan_petit_dejeuner, plan_dejeuner, plan_gouter, plan_repas) VALUES ($id_chef, datetime($jour, "localtime"), $petit_dejeuner, $dejeuner, $gouter, $repas)', {
         $id_chef: req.body.id_chef,
         $jour: req.body.jour,
         $petit_dejeuner: req.body.petit_dejeuner,
         $dejeuner: req.body.dejeuner,
         $gouter: req.body.gouter,
         $repas: req.body.repas
-    }, (err, row) => {
+    }, (err) => {
         if(err) {
             return res.json(err).status(500);
         } else {
-            return res.json({id: row.plan_id}).status(200);
+            return res.status(200);
         }
     })
 })
 
 routes.put('/planning/:id_chef/:id', auth.authenticate(), (req, res) => {
-    db.get('UPDATE planning SET plan_petit_dejeuner = $petit_dejeuner, plan_dejeuner = $dejeuner, plan_gouter = $gouter, plan_repas = $repas WHERE plan_id_chef=$id_chef AND plan_id=$id returning plan_id', {
+    db.run('UPDATE planning SET plan_petit_dejeuner = $petit_dejeuner, plan_dejeuner = $dejeuner, plan_gouter = $gouter, plan_repas = $repas WHERE plan_id_chef=$id_chef AND plan_id=$id', {
         $petit_dejeuner: req.body.petit_dejeuner,
         $dejeuner: req.body.dejeuner,
         $gouter: req.body.gouter,
         $repas: req.body.repas,
         $id_chef: req.params.id_chef,
         $id: req.params.id
-    }, (err, row) => {
+    }, (err) => {
         if(err) {
             return res.json(err).status(500);
         } else {
-            return res.json({id: row.plan_id}).status(200);
+            return res.status(200);
+        }
+    })
+})
+
+routes.delete('/planning/:id_chef/:id', auth.authenticate(), (req, res) => {
+    db.run('DELETE FROM planning WHERE plan_id_chef=$id_chef AND plan_id=$id', {
+        $id_chef: req.params.id_chef,
+        $id: req.params.id
+    }, (err) => {
+        if(err) {
+            return res.json(err).status(500);
+        } else {
+            return res.status(200);
         }
     })
 })
